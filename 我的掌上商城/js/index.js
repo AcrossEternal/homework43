1f9feb8ec442d4cc4767da50482cf8e9c16bdee3
1305779886 <1305779886@qq.com>
2018-11-01 23:58:37 +0800

    ...

--- 我的掌上商城/js/index.js
+++ 我的掌上商城/js/index.js
@@ -35,7 +35,6 @@ function search(){
 }
 // 轮播
 
-// 获取一组带图像的超链接
 var imagesA = document.getElementById("images").children;
 //获取一组li文本
 var txtList = document.querySelector(".txt-box").children;
@@ -135,33 +134,33 @@ function rightImgGo(){
     rightImg();
     changeImg();
 }
-// 倒计时
-function downTime(){
-    // 需要倒计时的时间
-    var time=5*60*60;
-    var timer=null;
-    // 操作dom
-    var skTime=document.querySelector('.sk_time');
-    // 所有的SPAN
-    var spans=skTime.querySelectorAll('span');
-    timer=setInterval(function){
-        if(time<=0){
-            clearInterval(timer);
-            return false;
-        }
-        time --;
-        // 格式化
-        var h=Math.floor(time/3600);
-        var m=Math.floor(time%3600/60);
-        var s=time%60;
-        console.log(h);
-        console.log(m);
-        console.log(s);
-        spans[0].innerHTML=Math.floor(h/10);
-        spans[1].innerHTML=h%10;
-        spans[3].innerHTML=Math.floor(m/10);
-        spans[4].innerHTML=m%10;
-        spans[6].innerHTML=Math.floor(s/10);
-        spans[7].innerHTML=s%10;
-    },1000);
-}
+// // 倒计时
+// function downTime(){
+//     // 需要倒计时的时间
+//     var time=5*60*60;
+//     var timer=null;
+//     // 操作dom
+//     var skTime=document.querySelector('.sk_time');
+//     // 所有的SPAN
+//     var spans=skTime.querySelectorAll('span');
+//     timer=setInterval(function){
+//         if(time<=0){
+//             clearInterval(timer);
+//             return false;
+//         }
+//         time --;
+//         // 格式化
+//         var h=Math.floor(time/3600);
+//         var m=Math.floor(time%3600/60);
+//         var s=time%60;
+//         console.log(h);
+//         console.log(m);
+//         console.log(s);
+//         spans[0].innerHTML=Math.floor(h/10);
+//         spans[1].innerHTML=h%10;
+//         spans[3].innerHTML=Math.floor(m/10);
+//         spans[4].innerHTML=m%10;
+//         spans[6].innerHTML=Math.floor(s/10);
+//         spans[7].innerHTML=s%10;
+//     },1000);
+// }
